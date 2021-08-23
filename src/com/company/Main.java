9cfed49aeabf87e44c5969baa19ec4455938fd25
InnaTovstik <kitimail69@gmail.com>
2021-08-23 21:40:37 +0300

    Reformat code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,18 +11,18 @@ public class Main {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         try {
             //1.Предложили пользователю ввести число. Это число будет длиной массива целых чисел
-            int lengthArray = readInteger(buffer, "Please Enter The Number To Length Array: ");
+            int lengthArray = readInteger(buffer, "Please Enter The Number For Length Of An Array Of Integers: ");
             int[] array = new int[lengthArray];
             //2.	Предложили пользователю заполнить массив.
             for (int i = 0; i < lengthArray; i++) {
-                array[i] = readInteger(buffer, "Enter The Value Of The Array Element a[" + i + "] = ");
+                array[i] = readInteger(buffer, "Please Enter The Value Of The Array Element a[" + i + "] = ");
             }
             //Предложили пользователю выбрать 2 числа из массива по индексу
             int[] ar = readIndex(buffer, array);
             // Разделили первое число на второе
             int rez = ar[0] / ar[1]; // ожидаем ArithmeticException только для int
             // для получения результата double приводим делимое к double
-            System.out.println("rezult = " + ((double) ar[0] / ar[1]));
+            System.out.println(ar[0] + " / " + ar[1] + " = " + ((double) ar[0] / ar[1]));
         } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
             e.printStackTrace();
         }
@@ -52,7 +52,7 @@ public class Main {
         int maxTries = 5; // ограничим 5 попытками
         int j = 0;
         int[] ar = new int[2];
-        String prompt = "Select Index Of Number: ";
+        String prompt = "Please Select Index Of Number: ";
         while (true) {
             try {
                 while (j < 2) {
